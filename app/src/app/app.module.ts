06898151128e5b2308f35d90e02ee5ab215d1349
Fabricio Magalhães <fabricioms.dev@gmail.com>
2019-12-12 16:05:12 -0300

    [ change ] Remove app module component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,22 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HTTP_INTERCEPTORS } from "@angular/common/http"; 
 import { MaterialModule } from './shared/material/angular-material.module';
 import { TradeModule } from './trade/trade.module';
 import { BankModule } from './bank/bank.module';
-import { UserGreetingComponent } from './user-greeting/user-greeting.component';
+import { LoginModule } from './login/login.module';
+import { SharedModule } from './shared/shared.module';
+import { AuthGuardService } from './auth/auth-guard.service';
+import { AccountModule } from './account/account.module';
+import { BasicHttpInterceptor } from './http/basic-http-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    MaterialModule,
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    SharedModule,
     TradeModule,
-    BankModule
+    BankModule,
+    LoginModule,
+    AccountModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    AuthGuardService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: BasicHttpInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
