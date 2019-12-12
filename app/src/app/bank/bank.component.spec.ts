196ae69b5b6febaf5224d6370ba52e25c1955d4d
Fabricio Magalhães <fabricioms.dev@gmail.com>
2019-12-12 13:10:32 -0300

    [ change ] Added basics tests of bank component

--- app/src/app/bank/bank.component.spec.ts
+++ app/src/app/bank/bank.component.spec.ts
@@ -3,6 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BankComponent } from './bank.component';
 import { MaterialModule } from '../shared/material/angular-material.module';
 
+// left for brevity
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 describe('BankComponent', () => {
   let component: BankComponent;
   let fixture: ComponentFixture<BankComponent>;
@@ -10,7 +13,7 @@ describe('BankComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [BankComponent],
-      imports: [MaterialModule]
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -23,4 +26,51 @@ describe('BankComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('bank component has the default params', () => {
+    expect(component.accountData != null).toBe(true);
+    expect(component.accountData.user != null).toBe(true);
+    expect(component.accountData.balance == 0).toBe(true);
+  })
+
+  it('should has default methods for operate', () => {
+    expect(component.canUserRemoveValueOfAccount != null).toBe(true);
+    expect(component.makeAnAportOnAccount != null).toBe(true);
+    expect(component.makeAnRemoveOnAccount != null).toBe(true);
+  });
+
+  it("should make add value in account balance", () => {
+    const value = 50;
+
+    component.makeAnAportOnAccount(value);
+
+    expect(component.accountData.balance == 50).toBe(true);
+  });
+
+  it("should make remove value in account balance", () => {
+    const value = 50;
+
+    component.makeAnAportOnAccount(value);
+    expect(component.accountData.balance == 50).toBe(true);
+
+    component.makeAnRemoveOnAccount(value);
+    expect(component.accountData.balance == 0).toBe(true);
+  });
+
+  it("should not lets user remove a value of account if account balance is less than value which removed", () => {
+    const value = 50;
+
+    component.makeAnRemoveOnAccount(value);
+
+    expect(component.accountData.balance == 0).toBe(true);
+  });
+
+  it("should not lets user remove a value of account if account balance is less than value which removed", () => {
+    const value = 50;
+
+    component.makeAnRemoveOnAccount(value);
+
+    expect(component.accountData.balance == 0).toBe(true);
+  });
+
 });
--- app/src/app/bank/bank.component.ts
+++ app/src/app/bank/bank.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './../models/user';
 
 @Component({
-  selector: 'bank',
-  templateUrl: './bank.component.html',
-  styleUrls: ['./bank.component.scss']
+  selector: "bank",
+  templateUrl: "./bank.component.html",
+  styleUrls: ["./bank.component.scss"]
 })
 export class BankComponent implements OnInit {
+  accountData = {
+    user: new User(),
+    balance: 0
+  };
+  valueToAction = 0;
+  errorMessages = {
+    userHasNotSuficientMoney:
+      "Você não tem dinheiro o suficiente para executar está ação!"
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  presentToast(message, color = "default", duration = 3000) {}
+
+  makeAnAportOnAccount(value: number) {
+    this.accountData.balance += value;
   }
 
+  canUserRemoveValueOfAccount(value: number) {
+    return this.accountData.balance >= value;
+  }
+
+  makeAnRemoveOnAccount(value: number) {
+    if (this.canUserRemoveValueOfAccount(value)) {
+      this.accountData.balance -= value;
+    } else {
+      this.presentToast(this.errorMessages.userHasNotSuficientMoney);
+    }
+  }
+
+  ngOnInit() {}
 }
