da791242803d013294eefafeff939bda53dd1a6e
Fabricio Magalhães <fabricioms.dev@gmail.com>
2019-12-13 19:28:02 -0300

    [ change ] Backend: Begin backend tests

--- backend/services/transactions/orders/tests.py
+++ backend/services/transactions/orders/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from main.tests import (
+    TestResourceRouteExist,
+    TestUserNotLoggedCanNotPost,
+    TestUserLoggedCanPost,
+    TestUserLoggedCanUpdate,
+    TestUserLoggedCanDelete,
+    TestCase)
 
-# Create your tests here.
+class OrderRestApiTest(
+        TestResourceRouteExist,
+        TestUserNotLoggedCanNotPost,
+        TestUserLoggedCanPost,
+        TestUserLoggedCanUpdate,
+        TestUserLoggedCanDelete,
+        TestCase):
+
+    data_for_test = {
+        'quantity': 100,
+        'sigla': 'PETR4',
+        'order_type': 'bought'
+    }
+    data_updated_for_test = {
+        'quantity': 100,
+        'sigla': 'PETR4',
+        'order_type': 'sell'
+    }
+    url = '/api/orders/'
+
+
+class OperationRestApiTest(
+        TestResourceRouteExist,
+        TestUserNotLoggedCanNotPost,
+        TestUserLoggedCanPost,
+        TestUserLoggedCanUpdate,
+        TestUserLoggedCanDelete,
+        TestCase):
+
+    data_for_test = {
+        'value': 100,
+        'operation_type': 'deposit'
+    }
+    data_updated_for_test = {
+        'value': 100,
+        'operation_type': 'draw'
+    }
+    url = '/api/operation/'
