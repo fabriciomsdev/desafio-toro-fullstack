ff4d609af6c1404e99c54dd54030f5404d0f3995
Fabricio Magalhães <fabricioms.dev@gmail.com>
2019-12-13 19:26:13 -0300

    [ fix ] Backend: Fix and Change api url resources path

--- backend/services/transactions/main/urls.py
+++ backend/services/transactions/main/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from orders.views import index;
 
 urlpatterns = [
-    path('', index),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('api/', include('orders.urls'))
 ]
