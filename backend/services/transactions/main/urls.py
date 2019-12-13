9d1cda0a677dd751f1353328ff2f923a06ac8aba
Fabricio Magalhães <fabricioms.dev@gmail.com>
2019-12-12 22:31:34 -0300

    [ change ] Setup begin urls to init app

--- backend/services/transactions/main/urls.py
+++ backend/services/transactions/main/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from orders.views import index
+from django.urls import path, include
+from orders.views import index;
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
